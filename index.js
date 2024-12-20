f6f5fc6d05ca7407dd122ab5f17ed3e9d1db9432
Adithyasanker2002 <sankukuttan149@gmail.com>
2024-12-20 13:10:58 +0530

    third-commit

--- index.js
+++ index.js
@@ -1,53 +1,13 @@
-import express from 'express'
-import { config } from 'dotenv'
-import cors from 'cors'
-import cookieParser from "cookie-parser";
-import fileUpload from "express-fileupload";
-import { dbConnection } from './database/dbConnection.js';
-import messageRouter from './router/messageRouter.js'
-import {errorMiddleware} from './middlewares/errorMiddleware.js'
-import userRouter from "./router/userRouter.js";
-import appointmentRouter from './router/appointmentRouter.js'
+import app from "./app.js";
 import cloudinary from "cloudinary";
 
-const app = express()
-config({path:'./.env'})
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 
-app.use(
-    cors({
-      origin: [process.env.FRONDEND_URL, process.env.DASHBOARD_URL],
-      method: ["GET", "POST", "DELETE", "PUT"],
-      credentials: true,
-    })
-  );
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(
-    fileUpload({
-      useTempFiles: true,
-      tempFileDir: "/tmp/",
-    })
-  );
-  app.use(messageRouter);
-  app.use(userRouter);
-  app.use(appointmentRouter);
-  cloudinary.v2.config({
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
-    });
-  
-  app.listen(process.env.PORT,()=>{
-      console.log(`Server running on ${process.env.PORT}`);
-      
-  })
- 
-
-dbConnection()
-
-app.use(errorMiddleware)
-export default app
-
-// "/api/v1/user"
+app.listen(process.env.PORT,()=>{
+    console.log(`Server running on ${process.env.PORT}`);
+    
+})
